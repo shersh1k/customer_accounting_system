2c1f829aa25d427ed37ac2cdf72aeb8208c33c63
Шершнёв Андрей Юрьевич <andrei.sharshneu@mr.loc>
2019-07-30 14:12:07 +0300

    низя мутировать состояние

--- client/src/store/counter/reducers.ts
+++ client/src/store/counter/reducers.ts
@@ -7,9 +7,9 @@ const initialState = {
 export function counterReducer(state = initialState, action: any) {
   switch (action.type) {
     case COUNTER_DECREMENT:
-      return { ...state, count: --state.count };
+      return { ...state, count: state.count - 1 };
     case COUNTER_INCREMENT:
-      return { ...state, count: ++state.count };
+      return { ...state, count: state.count + 1 };
     default:
       return state;
   }
