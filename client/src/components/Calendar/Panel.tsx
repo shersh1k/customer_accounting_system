832752aea16cb796cfa8b4b2d0c4c0e73386772b
shersh1k <shershnev942@gmail.com>
2019-08-28 23:44:15 +0300

    buttons in calendat

--- client/src/components/Calendar/Panel.tsx
+++ client/src/components/Calendar/Panel.tsx
@@ -2,23 +2,26 @@ import React, { Component, MouseEvent } from 'react';
 import { Button } from '@material-ui/core';
 import { setViewType } from '../../store/calendar/actions';
 import { useDispatch } from 'react-redux';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
 
 export default function Panel() {
   const dispatch = useDispatch();
   return (
     <>
-      <span>Сегодня</span>
       <div /* className={panel.direction} */>
-        <Button onClick={() => {} /* this.props.setDateRange('prev') */}>prev</Button>
-        <div /* className={panel.dateRange} */ onClick={() => {} /*  this.props.setSelection() */}>
-          {/* {DateFormatter(this.props.viewType, this.props.dateRange)} */}
-        </div>
-        <Button onClick={() => {} /* this.props.setDateRange('next') */}>next</Button>
+        <Button onClick={() => {} /* this.props.setDateRange('prev') */}>
+          <ChevronLeft />
+        </Button>
+        <Button /* className={panel.dateRange} */ onClick={() => {} /*  this.props.setSelection() */}>Сегодня</Button>
+        <Button onClick={() => {} /* this.props.setDateRange('next') */}>
+          <ChevronRight />
+        </Button>
       </div>
 
-      <Button onClick={() => dispatch(setViewType('Week'))}>Week</Button>
-      <Button onClick={() => dispatch(setViewType('Month'))}>Month</Button>
-      <Button onClick={() => dispatch(setViewType('Year'))}>Year</Button>
+      <Button onClick={() => dispatch(setViewType('Week'))}>Неделя</Button>
+      <Button onClick={() => dispatch(setViewType('Month'))}>Месяц</Button>
+      <Button onClick={() => dispatch(setViewType('Year'))}>Год</Button>
     </>
   );
 }
